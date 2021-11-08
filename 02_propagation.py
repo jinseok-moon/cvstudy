55e2ca59ebbd043abbfa82d59fe339d9526b02de
Jinseok Moon <jinseokm38@gmail.com>
2021-11-08 22:01:01 +0900

    함수 추가
    cost함수, sigma diff 추가
    나중에 미분 계산식 추가 예정

--- 02_propagation.py
+++ 02_propagation.py
@@ -27,8 +27,11 @@
 # +
 import numpy as np
 
-def sigmoid(x):
-    return 1 / (1 +np.exp(-x))
+def sigmoid(x, diff=False):
+    if diff==True:
+        return sigmoid(x)*(1-sigmoid(x));
+    else:
+        return 1 / (1 +np.exp(-x))
 
 # Input layer
 x_input = np.array([2, 4])  # x1, x2
@@ -40,7 +43,6 @@ w1 = np.array([0.7, 0.3]) # w11, w21
 w2 = np.array([0.2, 0.4]) # w12, w22
 
 W = np.array([w1, w2])
-b = np.array([b1, b1])
 g1x = np.dot(W, x)
 h1x = sigmoid(g1x)
 
@@ -54,13 +56,14 @@ w3 = np.array([0.1, 0.5]) # w41, w42
 w4 = np.array([0.8, 0.4]) # w51, w52
 
 W_ = np.array([w3, w4])
-b_ = np.array([b2, b2])
 g2x = np.dot(W_, h1x)
 h2x = sigmoid(g2x)
 y_predict = h2x  # [y1p, y2p]
 
 print("g2 =",g2x) # g2 = [0.5334847  1.09700809]
 print("h2 =", h2x) # h2 = [0.63029549 0.74969909]
+
+
 # -
 
 # \begin{equation}\label{eqn:eq1}
@@ -84,3 +87,10 @@ print("h2 =", h2x) # h2 = [0.63029549 0.74969909]
 
 # ## 비용함수 (Cost function)
 # 우리는 퍼셉트론의 순전파/역전파를 이용해서 가중치들을 조정해 줄 것이다. 그리고 이 때 필요한 것이 바로 비용함수와 미분이다.
+# $\sigma(x)$의 미분은 $\sigma(x)(1-\sigma(x)$)로 나타낼 수 
+
+def cost(y_predict, y_true):
+    return (y_true-y_predict)*(y_true-y_predict)/2
+
+
+cost(y_predict, y_true)
