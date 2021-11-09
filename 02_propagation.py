ddedcd32f519ab2603f41b4443c4291533b2d760
Jinseok Moon <jinseokm38@gmail.com>
2021-11-09 22:35:05 +0900

    비용 함수 및 미분식 정리

--- 02_propagation.py
+++ 02_propagation.py
@@ -43,11 +43,11 @@ w1 = np.array([0.7, 0.3]) # w11, w21
 w2 = np.array([0.2, 0.4]) # w12, w22
 
 W = np.array([w1, w2])
-g1x = np.dot(W, x)
-h1x = sigmoid(g1x)
+g = np.dot(W, x)
+h = sigmoid(g)
 
-print("g1 =",g1x) # g1 = [2.6 2. ]
-print("h1 =", h1x) # h1 = [0.93086158 0.88079708]
+print("g =",g) # g = [2.6 2. ]
+print("h =", h) # h = [0.93086158 0.88079708]
 
 # +
 # Second layer
@@ -55,13 +55,13 @@ print("h1 =", h1x) # h1 = [0.93086158 0.88079708]
 w3 = np.array([0.1, 0.5]) # w41, w42
 w4 = np.array([0.8, 0.4]) # w51, w52
 
-W_ = np.array([w3, w4])
-g2x = np.dot(W_, h1x)
-h2x = sigmoid(g2x)
-y_predict = h2x  # [y1p, y2p]
+W_2 = np.array([w3, w4])
+z = np.dot(W_2, h)
+y = sigmoid(z)
+y_predict = y  # [y1p, y2p]
 
-print("g2 =",g2x) # g2 = [0.5334847  1.09700809]
-print("h2 =", h2x) # h2 = [0.63029549 0.74969909]
+print("z =", z) # g2 = [0.5334847  1.09700809]
+print("y =", y) # h2 = [0.63029549 0.74969909]
 
 
 # -
@@ -80,17 +80,46 @@ print("h2 =", h2x) # h2 = [0.63029549 0.74969909]
 # \label{eqn:eq3} \hat{y} &= h(x)
 # \end{align}
 #
-#
-#
 
 # 최종 계산해보면, $\hat{y_1}=0.630, \hat{y_2}=0.750$을 얻을 수 있다. 실제 $y_1=0.8, y_2=0.4$라고 가정했을 때, 퍼셉트론에서 예측한 값과 오차가 발생하게 된다.
 
 # ## 비용함수 (Cost function)
 # 우리는 퍼셉트론의 순전파/역전파를 이용해서 가중치들을 조정해 줄 것이다. 그리고 이 때 필요한 것이 바로 비용함수와 미분이다.
-# $\sigma(x)$의 미분은 $\sigma(x)(1-\sigma(x)$)로 나타낼 수 
+# $\sigma(x)$의 미분은 $\sigma(x)(1-\sigma(x)$)로 나타낼 수 있다. 유도식은 추후 추가.
+# 비용함수는 C는 아래와 같이 정의한다. 
+# \begin{align}
+# \label{eqn:eq2} C &= {1 \over 2} \left[ (\hat{y}_1 - y_1)^2 + (\hat{y}_2 - y_2)^2\right]
+# \end{align}
 
-def cost(y_predict, y_true):
-    return (y_true-y_predict)*(y_true-y_predict)/2
+def cost(y_true, y_predict, length):
+    cost = 0
+    for n in range(length):
+        cost += 1/2*(y_true[n]-y_predict[n])**2
+    return cost
 
 
-cost(y_predict, y_true)
+cost(y_predict, y_true, len(y_true))
+
+# \begin{equation}\label{eqn:eq1}
+# \begin{split}
+# z &= W_2^T h, & {dz \over dW_2}&= h\\[5pt]
+# \left( \begin{matrix}  z_{1} \newline z_{2} \end{matrix} \right)  &=  \left( \begin{matrix}  w_{31} & w_{41} \newline w_{32} & w_{42} \end{matrix} \right) \left( \begin{matrix}  h_{1} \newline h_{2} \end{matrix} \right) \\[5pt]
+# z_1 &= w_{31}h_1 + w_{41}h_2 \\[5pt]
+# z_2 &= w_{32}h_1 + w_{42}h_2 \\[5pt]
+# {dz_1 \over dw_{31}}&= h_1, \enspace {dz_1 \over dw_{41}}= h_2 \\[5pt]
+# {dz_2 \over dw_{32}}&= h_1, \enspace {dz_1 \over dw_{42}}= h_2 \\[5pt]
+# \end{split}
+# \end{equation}
+#
+# 이번에는 활성화 함수로 계단함수가 아닌 sigmoid function $\sigma(x)$를 이용한다.
+#
+# \begin{align}
+# \label{eqn:eq2} h(x) &= \sigma(x) = {1 \over 1 + e^{-x}} \\[5pt]
+# \label{eqn:eq3} \hat{y} &= h(x)
+# \end{align}
+#
+#
+#
+
+# ## References
+# [<sup id="fn1">1</sup>](#fn1-back) 선형대수와 통계학으로 배우는 머신러닝 with Python. https://github.com/bjpublic/MachineLearning
